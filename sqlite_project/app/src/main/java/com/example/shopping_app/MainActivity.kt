73817717bf4911f798b502583658ab32bea9513c
Retrobitti <PekkaParviainen@protonmail.com>
2024-03-25 19:44:52 +0200

    Added functionality
    Created a button that opens Dialog for adding a new item to database. Also added a list of all items in database to MainActivity.

--- sqlite_project/app/src/main/java/com/example/shopping_app/MainActivity.kt
+++ sqlite_project/app/src/main/java/com/example/shopping_app/MainActivity.kt
@@ -1,21 +1,31 @@
 package com.example.shopping_app
 
+
+import android.app.Application
 import android.os.Bundle
-import android.os.PersistableBundle
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-
-import com.example.shopping_app.database.*
-import  androidx.lifecycle.ViewModelProvider
+import androidx.compose.ui.unit.dp
+import com.example.shopping_app.components.AddItemDialog
+import com.example.shopping_app.database.ShoppingListItem
+import com.example.shopping_app.database.ShoppingListViewModel
+import com.example.shopping_app.database.ShoppingListViewModelFactory
 
 class MainActivity : ComponentActivity() {
     private lateinit var viewModel: ShoppingListViewModel
@@ -23,22 +33,38 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        viewModel = ViewModelProvider(this, ShoppingListViewModelFactory(application))[ShoppingListViewModel::class.java]
+        // Initialize ViewModel
+        viewModel = ShoppingListViewModelFactory(application).create(ShoppingListViewModel::class.java)
 
-        setContent{
-            ShoppingApp()
+        setContent {
+            ShoppingListApp(viewModel)
         }
     }
 }
 
-
 @Composable
-fun ShoppingApp(){
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ){
-        Text(text = "Testing")
+fun ShoppingListApp(viewModel: ShoppingListViewModel) {
+    var isAddItemDialogOpen by remember { mutableStateOf(false) }
+    val shoppingListItems by viewModel.readAllData.observeAsState()
+    Surface(color = MaterialTheme.colorScheme.background) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            shoppingListItems?.let { items ->
+                items.forEach { item ->
+                    Text(text = item.itemName)
+                }
+            }
+            Button(onClick = { isAddItemDialogOpen = true }) {
+                Text("Add Item")
+            }
+
+            if (isAddItemDialogOpen) {
+                AddItemDialog(
+                    viewModel = viewModel,
+                    onCancel = { isAddItemDialogOpen = false }
+                )
+            }
+        }
     }
 }
+
 
